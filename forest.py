eaa17cfff3edee4cdb35f3a5fad1503b7b473f36
lukaszrados@gmail.com <lukaszrados@gmail.com>
2016-01-20 21:21:12 +0100

    Random Forest Algorithm

---
+++ forest.py
@@ -0,0 +1,51 @@
+# Partially based on https://www.kaggle.com/gustavodemari/sf-crime/san-francisco-crime-classification/notebook
+
+import pandas as pd
+import numpy as np
+import time
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+
+start = time.time()
+
+# Wczytujemy dane (pomijamy kolumny: Descript, Resolution, Address)
+train = pd.read_csv('train.csv', parse_dates = ['Dates'], usecols = ['Dates', 'Category', 'DayOfWeek', 'PdDistrict', 'X', 'Y'], index_col = False)
+test = pd.read_csv('test.csv', parse_dates = ['Dates'], usecols = ['Id', 'Dates', 'DayOfWeek', 'PdDistrict', 'X', 'Y'], index_col = False)
+
+# Z daty zachowujemy jedynie godzinę
+train['Dates'] = train['Dates'].dt.hour
+test['Dates'] = test['Dates'].dt.hour
+
+# Przypisuje posterunkom wartości numeryczne 0-N
+districtEncoder = LabelEncoder()
+train['PdDistrict'] = districtEncoder.fit_transform(train['PdDistrict'])    
+test['PdDistrict'] = districtEncoder.fit_transform(test['PdDistrict'])
+
+# To samo dla dni tygodnia (0-6)
+dayEncoder = LabelEncoder()
+train['DayOfWeek'] = dayEncoder.fit_transform(train['DayOfWeek'])
+test['DayOfWeek'] = dayEncoder.fit_transform(test['DayOfWeek'])
+
+# Oraz dla kategorii (tego encodera użytwać będziemy jeszcze do odwrotnej transformacji)
+categoryEncoder = LabelEncoder()
+train['Category'] = categoryEncoder.fit_transform(train['Category'])
+
+learnColumns = list(train.columns.values)
+learnColumns.remove('Category')
+
+# Algorytm Random Forest (n_estimators = liczba drzew w lesie)
+clf = RandomForestClassifier(n_estimators = 12)
+clf.fit(train[learnColumns], train['Category'])
+
+# Predykcja kategorii 
+test['Category'] = clf.predict(test[learnColumns])
+
+end = time.time()
+
+print('Time: ' + str(end - start) + ' s')
+
+# Transformujemy indeksy do nazw kategorii
+test['Category'] = categoryEncoder.inverse_transform(test['Category'])
+
+# Zapis do CSV
+test.to_csv('output.csv', index = False)
--- stats.py
+++ stats.py
@@ -2,26 +2,55 @@ import csv
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+import pyradbas as pyrb
 
+NUMBER_OF_CRIMES = 10000
+INCLUDED_CRIMES = ['LARCENY/THEFT', 'VANDALISM', 'ROBBERY', 'VEHICLE THEFT', 'ASSAULT', 'DRUNKENNESS']
 
-def importDataFromFile(filename, usedColumns):
+def importDataFromFile(filename, usedColumns, targetColumn):
 
-    crimes = dict()
+    data = []
+    target = []
+    no = 0
 
     with open(filename, "r") as csvfile:
 
         for line in csv.reader(csvfile, skipinitialspace=True):
-            columns = list(line[i] for i in usedColumns)
-            hour = int(line[0].split(' ')[1].split(':')[0])
-            crime = columns[1]
-            
-            if crime in crimes:
-                crimes[crime][hour] += 1
-            else:
-                crimes[crime] = [0] * 24
-                crimes[crime][hour] = 1
 
-    return crimes
+            if line[targetColumn] not in INCLUDED_CRIMES:
+                continue
+
+            columns = list(line[i] for i in usedColumns)
+            columns[0] = int(columns[0].split(' ')[1].split(':')[0])
+            columns[1] = float(columns[1])
+            columns[2] = float(columns[2])
+
+            data.append(columns)
+            target.append(INCLUDED_CRIMES.index(line[1]))
+
+            no += 1
+
+            if no >= NUMBER_OF_CRIMES:
+                break
+
+    return [data, target]
+
+
+def countCrimesPerHour(crimes):
+
+    crimesPerHour = dict()
+
+    for line in crimes:
+        hour = int(line[0].split(' ')[1].split(':')[0])
+        crime = line[1]
+
+        if crime in crimesPerHour:
+            crimesPerHour[crime][hour] += 1
+        else:
+            crimesPerHour[crime] = [0] * 24
+            crimesPerHour[crime][hour] = 1
+
+    return crimesPerHour
 
 
 def plotCrimesPerHour(crimes):
@@ -38,6 +67,24 @@ def plotCityMap(crimes):
 
 if __name__ == "__main__":
 
-    crimes = importDataFromFile("train.csv", [ 0, 1, 7, 8 ])
-    plotCrimesPerHour(crimes)
-    plotCityMap(crimes)
+    start = time.time()
+
+    [data, target] = importDataFromFile("train.csv", [ 0, 7, 8 ], 1)
+    [data, target] = [np.array(data).reshape(len(data), 3), np.array(target).reshape(len(target), 1)]
+
+    net = pyrb.train_ols(data, target, 10e-8, 0.8, verbose=True)
+    S = net.sim(data)
+
+    errors = 0
+    count = 0
+
+    for i in range(len(target)):
+        count += 1
+
+        if int(round(S[i][0])) != target[i]:
+            errors += 1
+
+    end = time.time()
+
+    print('Learning error: ' + str(round((errors / count) * 100)) + '%')
+    print('Time: ' + str(end - start) + ' seconds')
