c3cb82a1ef14ea967bfbb0af637070cca74ecf16
lukaszrados@gmail.com <lukaszrados@gmail.com>
2016-01-25 20:04:04 +0100

    RF Algorithm: final version

--- forest.py
+++ forest.py
@@ -1,24 +1,59 @@
-# Partially based on https://www.kaggle.com/gustavodemari/sf-crime/san-francisco-crime-classification/notebook
-
 import pandas as pd
 import numpy as np
 import time
+import csv
+import seaborn as sns
+from pylab import savefig
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 
 start = time.time()
 
 # Wczytujemy dane (pomijamy kolumny: Descript, Resolution, Address)
-train = pd.read_csv('train.csv', parse_dates = ['Dates'], usecols = ['Dates', 'Category', 'DayOfWeek', 'PdDistrict', 'X', 'Y'], index_col = False)
-test = pd.read_csv('test.csv', parse_dates = ['Dates'], usecols = ['Id', 'Dates', 'DayOfWeek', 'PdDistrict', 'X', 'Y'], index_col = False)
+train = pd.read_csv('train.csv', parse_dates = ['Dates'], index_col = False)
+test = pd.read_csv('test.csv', parse_dates = ['Dates'], index_col = False)
 
-# Z daty zachowujemy jedynie godzinę
-train['Dates'] = train['Dates'].dt.hour
-test['Dates'] = test['Dates'].dt.hour
+'''
+plt.figure()
+fig = train['Category'].value_counts().plot(kind='barh', figsize=(18,10), color='c').get_figure()
+fig.savefig('categories.png')
 
-# Przypisuje posterunkom wartości numeryczne 0-N
+plt.figure()
+fig2 = train['DayOfWeek'].value_counts().plot(kind='bar', figsize=(15,10), color='c').get_figure();
+fig2.savefig('days.png')
+'''
+
+# Z daty zachowujemy jedynie godzinę i miesiac
+train['Hour'] = train['Dates'].dt.hour
+test['Hour'] = test['Dates'].dt.hour
+
+train['Month'] = train['Dates'].dt.month
+test['Month'] = test['Dates'].dt.month
+
+train['Year'] = train['Dates'].dt.year
+test['Year'] = test['Dates'].dt.year
+
+train['Time'] = train['Dates'].dt.hour * 60 + train['Dates'].dt.minute
+test['Time'] = test['Dates'].dt.hour * 60 + test['Dates'].dt.minute
+
+# Informacja, czy przestępstwo miało miejsce na skrzyżowaniu
+train['Corner'] = train['Address'].str.contains('/').map(int)
+test['Corner'] = test['Address'].str.contains('/').map(int)
+
+# Informacja, czy jest w apartamencie
+train['Block'] = train['Address'].str.contains('Block').map(int)
+test['Block'] = test['Address'].str.contains('Block').map(int)
+
+'''
+ax = sns.countplot(x = train["PdDistrict"])
+ax.set(xlabel = 'Departament', ylabel = 'Liczba przestępstw')
+savefig('district.png')
+'''
+
+# Kodujemy posterunki na inty
 districtEncoder = LabelEncoder()
-train['PdDistrict'] = districtEncoder.fit_transform(train['PdDistrict'])    
+train['PdDistrict'] = districtEncoder.fit_transform(train['PdDistrict'])
 test['PdDistrict'] = districtEncoder.fit_transform(test['PdDistrict'])
 
 # To samo dla dni tygodnia (0-6)
@@ -26,26 +61,56 @@ dayEncoder = LabelEncoder()
 train['DayOfWeek'] = dayEncoder.fit_transform(train['DayOfWeek'])
 test['DayOfWeek'] = dayEncoder.fit_transform(test['DayOfWeek'])
 
-# Oraz dla kategorii (tego encodera użytwać będziemy jeszcze do odwrotnej transformacji)
+# Oraz dla kategorii (tego encodera używać będziemy jeszcze do odwrotnej transformacji)
 categoryEncoder = LabelEncoder()
 train['Category'] = categoryEncoder.fit_transform(train['Category'])
 
 learnColumns = list(train.columns.values)
+skipColumns = ['Dates', 'Descript', 'Resolution', 'Address']
+skipTestColumns = ['Dates', 'Address']
+
+for column in skipColumns:
+    learnColumns.remove(column)
+    train.pop(column)
+
+for column in skipTestColumns:
+    test.pop(column)
+
 learnColumns.remove('Category')
 
 # Algorytm Random Forest (n_estimators = liczba drzew w lesie)
-clf = RandomForestClassifier(n_estimators = 12)
+clf = RandomForestClassifier()
+clf.set_params(n_estimators = 52)
 clf.fit(train[learnColumns], train['Category'])
 
 # Predykcja kategorii 
-test['Category'] = clf.predict(test[learnColumns])
+#test['Category'] = clf.predict(test[learnColumns])
 
-end = time.time()
+print('\nSaving to csv:')
 
-print('Time: ' + str(end - start) + ' s')
+with open('output.csv', 'w', newline='') as output:
+    wr = csv.writer(output, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    wr.writerow(['Id'] + list(categoryEncoder.classes_))
 
-# Transformujemy indeksy do nazw kategorii
-test['Category'] = categoryEncoder.inverse_transform(test['Category'])
+    chunks = 9
+    perChunk = 100000
+    currentId = 0
 
-# Zapis do CSV
-test.to_csv('output.csv', index = False)
+    for j in range(chunks):
+        begin = j * perChunk
+        end = (j + 1) * perChunk
+        if end > len(test.index):
+            end = len(test.index)
+
+        predictions = clf.predict_proba(test[learnColumns][begin:end])
+
+        for i in range(len(predictions)):
+            wr.writerow([currentId] + predictions[i].tolist())
+            currentId += 1
+
+        print('Save: ' + str(j + 1) + ' / ' + str(chunks))
+
+
+
+print('Time: ' + str(time.time() - start) + ' s')
+
